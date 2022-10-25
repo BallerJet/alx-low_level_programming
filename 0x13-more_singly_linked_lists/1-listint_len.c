ccf7d7a324c49d007cffd5c884e567f4d71e2c0d
BallerJet <drjetliu@gmail.com>
2022-10-25 14:58:59 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,6 @@
-include "lists.h"
+#include "lists.h"
 #include <stdio.h>
+
 /**
  * listint_len - Returns the number of elements
  *               in a linked listint_t list.
@@ -16,6 +17,5 @@ size_t listint_len(const listint_t *h)
 		nodes++;
 		h = h->next;
 	}
-
 	return (nodes);
 }
